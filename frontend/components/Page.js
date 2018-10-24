1f6e2cd3877a5a5c79fef8ece4254e2589c973d7
Ranadive <Amogh.Ranadive@thomsonreuters.com>
2018-10-24 15:49:55 -0400

    basic setup

---
+++ frontend/components/Page.js
@@ -0,0 +1,17 @@
+import {Component} from 'react'
+import Header from '../components/Header'
+import Meta from '../components/Meta'
+
+class Page extends Component {
+    render() {
+        return (
+            <div>
+                <Meta />
+                <Header />
+                {this.props.children}
+            </div>
+        )
+    }
+}
+
+export default Page;
