8be452826abbc501948fa855c2a8139b8e3f188d
Ranadive <Amogh.Ranadive@thomsonreuters.com>
2018-11-05 11:30:03 -0500

    submit item

--- backend/src/index.js
+++ backend/src/index.js
@@ -8,7 +8,7 @@ server.start(
     {
         cors: {
             credentials: true,
-            progin: process.env.FRONTEND_URL,
+            origin: process.env.FRONTEND_URL,
         }
     }, 
     function onStart(deets) {
--- frontend/components/CreateItem.js
+++ frontend/components/CreateItem.js
@@ -1,17 +1,41 @@
 import React, { Component } from 'react';
 import { Mutation } from 'react-apollo';
 import gql from 'graphql-tag';
+import Router from 'next/router'
+
 import Form from './styles/Form';
 import formatMoney from '../lib/formatMoney';
+import Error from './ErrorMessage';
+
+
+const CREATE_ITEM_MUTATION = gql`
+    mutation CREATE_ITEM_MUTATION (
+        $title: String!
+        $description: String!
+        $price: Int!
+        $image: String
+        $largeImage: String      
+    ) {
+        createItem(
+            title: $title
+            description: $description
+            price: $price
+            image: $image
+            largeImage: $largeImage
+        ) {
+            id
+        }
+    }
+`;
 
 class CreateItem extends Component {
 
     state = {
-        title: '',
-        description: '',
+        title: 'Bike',
+        description: 'Its fun!',
         image: '',
         largeImage: '',
-        price: 0
+        price: 250.50
     };
 
     handleChange = (e) => {
@@ -22,52 +46,67 @@ class CreateItem extends Component {
 
     render() {
         return (
-            <Form onSubmit = {(e) => {
-                e.preventDefault();
-                console.log(this.state);
-            }}>
-                <fieldset>
-                    <label htmlFor="title">
-                        Title
-                        <input 
-                            type="text" 
-                            id="title" 
-                            name="title" 
-                            placeholder="Title" 
-                            value={this.state.title} 
-                            onChange={this.handleChange}
-                            required />
-                    </label>
+            <Mutation mutation={CREATE_ITEM_MUTATION} variables={this.state}>
+                {(createItem, { loading, error }) => (
+                    <Form onSubmit = {async e => {
+                        // stop the form from submitting
+                        e.preventDefault();
 
-                    <label htmlFor="price">
-                        Price
-                        <input 
-                            type="number" 
-                            id="price" 
-                            name="price" 
-                            placeholder="Price" 
-                            value={this.state.price} 
-                            onChange={this.handleChange}
-                            required />
-                    </label>
+                        // call the mutation
+                        const res = await createItem();
 
-                    <label htmlFor="description">
-                    Description
-                        <input 
-                            id="description" 
-                            name="description" 
-                            placeholder="Enter a description" 
-                            value={this.state.description} 
-                            onChange={this.handleChange}
-                            required />
-                    </label>
+                        // take them to teh single item page.
+                        Router.push({
+                            pathname: '/item',
+                            query: { id: res.data.createItem.id }
+                        })
+                    }}>
+                        <Error error={ error } />
+                        <fieldset disabled={loading} aria-busy= {loading}>
+                            <label htmlFor="title">
+                                Title
+                                <input 
+                                    type="text" 
+                                    id="title" 
+                                    name="title" 
+                                    placeholder="Title" 
+                                    value={this.state.title} 
+                                    onChange={this.handleChange}
+                                    required />
+                            </label>
 
-                    <button type="submit">Submit</button>
-                </fieldset>
-            </Form>
+                            <label htmlFor="price">
+                                Price
+                                <input 
+                                    type="number" 
+                                    id="price" 
+                                    name="price" 
+                                    placeholder="Price" 
+                                    value={this.state.price} 
+                                    onChange={this.handleChange}
+                                    required />
+                            </label>
+
+                            <label htmlFor="description">
+                            Description
+                                <input 
+                                    id="description" 
+                                    name="description" 
+                                    placeholder="Enter a description" 
+                                    value={this.state.description} 
+                                    onChange={this.handleChange}
+                                    required />
+                            </label>
+
+                            <button type="submit">Submit</button>
+                        </fieldset>
+                    </Form>
+                )}
+            </Mutation>
         )
     }
 
 }
 
 export default CreateItem;
+export { CREATE_ITEM_MUTATION };
